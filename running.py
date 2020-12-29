b73b3ea44d8ce0c6ca2b8595cd09245c7af652c5
Noah Osuolale <osuolale49@gmail.com>
2020-12-29 16:55:34 +0100

    Initial commit

---
+++ running.py
@@ -0,0 +1,13 @@
+import Flask as Flask
+
+app = Flask(__name__)
+
+def highest_random(limit):
+    highest = 0
+    for i in range(5):
+        r = randint(0, limit)
+        if r > highest:
+            highest = r
+    msg = "Hello {num:d}".format(num=highest)
+    return msg
+
